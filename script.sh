40aa9baaa88ba78076e09bd9a0ba2ac2e0e27cc0
Adam McCullough <merlinfmct87@gmail.com>
2020-09-11 18:42:06 -0700

    Make the how-to script more robust, as well as, y'know, correct

--- script.sh
+++ script.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+rm example.eventlog example.eventlog.trace.json
+stack install opentelemetry-extra
 stack clean
 stack build --ghc-options="-eventlog"
-stack run -- +RTS -T -l -olpub_example.eventlog
+find .stack-work/ -name render | tail -n 1 | xargs -I '{}' cp '{}' .
+./render ${@} +RTS -T -l -olexample.eventlog
+eventlog-to-chrome read example.eventlog
+cat example.eventlog.trace.json
